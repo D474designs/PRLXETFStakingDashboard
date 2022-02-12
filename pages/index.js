2b2a21f74b1d146a1304832c977cd28a7cdd2965
D474designs <jcazares117@gmail.com>
2022-02-11 23:23:01 -0700

    update dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import DashboardConcept from '@components/DashboardConcept'
 import Footer from '@components/Footer'
 
 export default function Home() {
