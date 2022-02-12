2d50ad108b44fb93399daa66c77b2b293bef4e87
D474designs <jcazares117@gmail.com>
2022-02-11 22:41:14 -0700

    update dashboard

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <meta charset="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
