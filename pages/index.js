1d126b6126f647470503516f6fe8a8d213667f93
D474designs <jcazares117@gmail.com>
2022-02-11 23:00:45 -0700

    update dashboard

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,6 @@ export default function Home() {
         </p>
 
         <div>
-          <meta charSet="UTF-8" />
-          <title>PRLX ETF - Dashboard</title>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" />
           <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css" /><link rel="stylesheet" href="./style.css" />
           {/* partial:index.partial.html */}
