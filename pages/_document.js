83575ad4ba1f559ec1c27938dfc27a49deaa7fde
D474designs <jcazares117@gmail.com>
2022-02-11 22:59:31 -0700

    update dashboard

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,6 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      // Agregamos el idioma que deseemos, aquí por ejemplo español.
       <Html lang="en">
         <Head />
         <body>
