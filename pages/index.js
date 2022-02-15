ef1c99725e871079e0a397d6f4a0f4f885e948f8
D474designs <jcazares117@gmail.com>
2022-02-15 00:00:22 -0700

    update dashboard

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,12 @@ export default function Home() {
       <Script
         src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js"
       />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      />
+      <Script
+        src="./script.js"
+      />
       <Head>
         <meta charSet="UTF-8" />
         <title>PRLX ETF - Dashboard</title>
