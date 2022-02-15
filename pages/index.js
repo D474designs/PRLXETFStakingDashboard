3078ea6bd929b4ba9ae2697a8f954f6fe90597fa
D474designs <jcazares117@gmail.com>
2022-02-14 22:48:04 -0700

    update dashboard

--- pages/index.js
+++ pages/index.js
@@ -346,8 +346,8 @@ export default function Home() {
         {/* partial */}
       </div>
       <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js'></script>
-      <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.jshttps://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'></script><script  src="./script.js"></script>
-
+      <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'></script><script src="./script.js"></script>
+      
       <Footer />
     </div>
   )
