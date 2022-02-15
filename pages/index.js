973c7852124b22e86e48dc9f2808c1dc1337bf8c
D474designs <jcazares117@gmail.com>
2022-02-14 23:47:12 -0700

    update dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import Script from 'next/script'
 
 export default function Home() {
   return (
     <div className="container">
-    <Script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js" strategy="lazyOnload" />
-    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
-    <Script src="./script.js" strategy="lazyOnload" />
       <Head>
         <meta charSet="UTF-8" />
         <title>PRLX ETF - Dashboard</title>
