bccd818b7b341aed6e643228238af2acc7f7bc57
D474designs <jcazares117@gmail.com>
2022-02-14 23:27:43 -0700

    update dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,15 @@ function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-<Script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js" strategy="lazyOnload" />
-<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
-<Script src="./script.js" strategy="lazyOnload" />
+export default function Home() {
+
+  return (
+    <>
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js"
+      />
+    </>
+  )
+}
 
 export default Application
