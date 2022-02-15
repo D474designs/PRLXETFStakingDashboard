022c5e093bfd14045c628f2ab856450fb90ea90a
D474designs <jcazares117@gmail.com>
2022-02-14 23:05:33 -0700

    update dashboard

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,7 @@ export default function Home() {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css" /><link rel="stylesheet" href="./style.css" />
 
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js'></script>
-        <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'></script><script src="./script.js"></script>
+        <script src="script.js"></script>
 
       </Head>
 
