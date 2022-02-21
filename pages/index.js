f1a8cd0cf0e20ed3f908f97e64e0746078449b8e
D474designs <jcazares117@gmail.com>
2022-02-21 15:35:11 -0700

    update dApp

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,11 @@ export default function Home() {
         src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js"
         strategy="beforeInteractive"
       />
-      {/*
+
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         strategy="beforeInteractive"
       />
-      */}
       <Script
         src="/dashboardConcept/script.js"
         strategy="beforeInteractive"
@@ -24,7 +23,9 @@ export default function Home() {
         <meta charSet="UTF-8" />
         <title>PRLX ETF - Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
+        {/*
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" />
+        */}
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css" />
       </Head>
 
