40df1846819c32322e0c5a895c811500696312d9
D474designs <jcazares117@gmail.com>
2022-02-21 14:28:55 -0700

    update dApp

--- public/dashboardConcept/script.js
+++ public/dashboardConcept/script.js
@@ -24,6 +24,20 @@ ethereum.on('accountsChanged', function (accounts) {
 
 let accounts = [];
 
+// txHash is a hex string
+// As with any RPC call, it may throw an error
+const txHash = await ethereum.request({
+  method: 'eth_sendTransaction',
+  params: [transactionParameters],
+});
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+  avatarId.style.display = 'block';
+}
+
 //Sending Ethereum to an address
 sendEthButton.addEventListener('click', () => {
   ethereum
@@ -47,32 +61,6 @@ sendEthButton.addEventListener('click', () => {
     .then((txHash) => console.log(txHash))
     .catch((error) => console.error);
 });
-
-const transactionParameters = {
-  nonce: '0x00', // ignored by MetaMask
-  gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
-  gas: '0x2710', // customizable by user during MetaMask confirmation.
-  to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
-  from: ethereum.selectedAddress, // must match user's active address.
-  value: '0x00', // Only required to send ether to the recipient from the initiating external account.
-  data:
-    '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
-  chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
-};
-
-// txHash is a hex string
-// As with any RPC call, it may throw an error
-const txHash = await ethereum.request({
-  method: 'eth_sendTransaction',
-  params: [transactionParameters],
-});
-
-async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  showAccount.innerHTML = account;
-  avatarId.style.display = 'block';
-}
 
 
 
