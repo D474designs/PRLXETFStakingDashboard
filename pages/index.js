8af33fbfa6441cbf3ef41b892487be45dec731f8
D474designs <jcazares117@gmail.com>
2022-03-02 17:08:00 -0700

    update dApp

--- pages/index.js
+++ pages/index.js
@@ -70,6 +70,8 @@ export default function Home() {
               Admin Options
               <button class="sendCrypto" style={{"background-color":"#ffc107","color":"#fff","-webkit-text-shadow":"-1px -1px 0 #000,    1px -1px 0 #000,    -1px 1px 0 #000,    1px 1px 0 #000","text-shadow":"-1px -1px 0 #000,    1px -1px 0 #000,    -1px 1px 0 #000,    1px 1px 0 #000"}}>&nbsp;
               Create Pool &nbsp;</button>
+              </p>
+              </div>
           </center>
 
           <div className="search position-relative text-center px-4 py-3 mt-2">
