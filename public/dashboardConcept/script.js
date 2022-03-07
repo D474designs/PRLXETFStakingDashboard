9fa9c47c84f999ba9779ec856b76d183edf01311
D474designs <jcazares117@gmail.com>
2022-03-07 16:20:06 -0700

    update dApp

--- public/dashboardConcept/script.js
+++ public/dashboardConcept/script.js
@@ -569,6 +569,8 @@ ethereumButton.addEventListener("click", () => {
   getAccount();
 });
 
+
+
 // Sending Ethereum to an address
 sendEthButton.addEventListener("click", () => {
   let receiverAddress = document.getElementById("receiver");
@@ -584,14 +586,92 @@ sendEthButton.addEventListener("click", () => {
   // Send a transaction
   wallet.sendTransaction(tx);
 });
-
+let receiverAddress = document.getElementById("receiver");
+let amountInEther = document.getElementById("amount");
 console.log(receiverAddress);
 console.log(amountInEther);
 
+
+
+/*
 // Creating a new staking pool
 sendEthButton2.addEventListener("click", () => {
 
+	let walletSigner = wallet.connect(window.ethersProvider)
+	window.ethersProvider.getGasPrice() // gasPrice
+
+	let private_key =
+  "41559d28e936dc92104ff30691519693fc753ffbee6251a611b9aa1878f12a4d"
+	let send_token_amount = "1"
+	let to_address = "0x4c10D2734Fb76D3236E522509181CC3Ba8DE0e80"
+	let send_address = "0xda27a282B5B6c5229699891CfA6b900A716539E6"
+	let gas_limit = "0x100000"
+	let wallet = new ethers.Wallet(private_key)
+	let walletSigner = wallet.connect(window.ethersProvider)
+	let contract_address = ""
+	window.ethersProvider = new ethers.providers.InfuraProvider("ropsten")
+
+	function send_token(
+  contract_address,
+  send_token_amount,
+  to_address,
+  send_account,
+  private_key
+) {
+  let wallet = new ethers.Wallet(private_key)
+  let walletSigner = wallet.connect(window.ethersProvider)
+
+  window.ethersProvider.getGasPrice().then((currentGasPrice) => {
+    let gas_price = ethers.utils.hexlify(parseInt(currentGasPrice))
+    console.log(`gas_price: ${gas_price}`)
+
+    if (contract_address) {
+      // general token send
+      let contract = new ethers.Contract(
+        contract_address,
+        send_abi,
+        walletSigner
+      )
+
+      // How many tokens?
+      let numberOfTokens = ethers.utils.parseUnits(send_token_amount, 18)
+      console.log(`numberOfTokens: ${numberOfTokens}`)
+
+      // Send tokens
+      contract.transfer(to_address, numberOfTokens).then((transferResult) => {
+        console.dir(transferResult)
+        alert("sent token")
+      })
+    } // ether send
+    else {
+      const tx = {
+        from: send_account,
+        to: to_address,
+        value: ethers.utils.parseEther(send_token_amount),
+        nonce: window.ethersProvider.getTransactionCount(
+          send_account,
+          "latest"
+        ),
+        gasLimit: ethers.utils.hexlify(gas_limit), // 100000
+        gasPrice: gas_price,
+      }
+      console.dir(tx)
+      try {
+        walletSigner.sendTransaction(tx).then((transaction) => {
+          console.dir(transaction)
+          alert("Send finished!")
+        })
+      } catch (error) {
+        alert("failed to send!!")
+      }
+    }
+  })
+}
+
 });
+*/
+
+
 
 // Editing an existing staking pool
 sendEthButton3.addEventListener("click", () => {
