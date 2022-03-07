1507795e8859de8b1713865fc0d0021e2e8dd92a
D474designs <jcazares117@gmail.com>
2022-03-07 15:53:38 -0700

    update dApp

--- public/dashboardConcept/script.js
+++ public/dashboardConcept/script.js
@@ -554,7 +554,7 @@ async function getAccount() {
 
   let account = await signer.getAddress();
 
-	let chainIds = await provider.getNetwork();
+	let chainIds = await signer.getChainId();
 	let chainId = chainIds.chainId;
 
 	showAccount.innerHTML = account;
@@ -573,11 +573,27 @@ ethereumButton.addEventListener("click", () => {
 
 //Sending Ethereum to an address
 sendEthButton.addEventListener('click', () => {
-	signer.getGasPrice();
-	receiver = document.getElementById("receiver").innerText;
-	amount = document.getElementById("amount").innerText;
+	let receiverAddress = document.getElementById("receiver").innerText;
+	let amountInEther = document.getElementById("amount").innerText;
+
+	let wallet = wallet.provider;
+
+	let tx = {
+	    to: receiverAddress,
+	    // Convert currency unit from ether to wei
+	    value: ethers.utils.parseEther(amountInEther)
+	}
+	// Send a transaction
+wallet.sendTransaction(tx)
+.then((txObj) => {
+    console.log('txHash', txObj.hash)
+    // => 0x9c172314a693b94853b49dc057cf1cb8e529f29ce0272f451eea8f5741aa9b58
+    // A transaction result can be checked in a etherscan with a transaction hash which can be obtained here.
+})
 
 });
+
+
 
 /*
 sendEthButton.addEventListener('click', () => {
