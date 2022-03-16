ff6bc322a2ffbdcc2a8305b53dba013b0437079c
D474designs <jcazares117@gmail.com>
2022-03-15 21:30:10 -0700

    update dapp~

--- public/dashboardConcept/script.js
+++ public/dashboardConcept/script.js
@@ -630,7 +630,7 @@ sendEthButton2.addEventListener("click", async () => {
 });
 
 // console.log(abi.add());
-console.log(contract.interface.functions.add);
+console.log(contract.interface.functions[0]);
 
 sendEthButton3.addEventListener("click", async () => {
   let pid = document.getElementById("poolId");
@@ -659,7 +659,7 @@ sendEthButton3.addEventListener("click", async () => {
 });
 
 // console.log(abi.set());
-console.log(contract.interface.functions.set);
+console.log(contract.interface.functions[9]);
 
 //Sending Ethereum to an address
 
