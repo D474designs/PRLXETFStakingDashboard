ebbe5d675f4c8344e86f3c87413eb20d69f1cde0
D474designs <jcazares117@gmail.com>
2022-03-17 13:35:41 -0700

    update dapp

--- public/dashboardConcept/script.js
+++ public/dashboardConcept/script.js
@@ -545,6 +545,8 @@ function verifyMeta() {
   }
 }
 
+verifyMeta();
+
 try {
   // MetaMask requires requesting permission to connect users accounts
   const provider = new ethers.providers.Web3Provider(window.ethereum);
