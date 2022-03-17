7f19911b9f174d49a554668510083592fcdd8c88
D474designs <jcazares117@gmail.com>
2022-03-16 23:20:25 -0700

    update dapp

--- public/dashboardConcept/script.js
+++ public/dashboardConcept/script.js
@@ -565,11 +565,15 @@ const contractAddress = "0x15daf22b26cce33cc5f7e08a9b54d84ecd26c3a2";
 // have read-only access to the Contract
 const contract = new ethers.Contract(contractAddress, abi, provider);
 
+
+
 async function getAccount() {
-  // MetaMask requires requesting permission to connect users accounts
-  provider.send("eth_requestAccounts", []);
+
+
 
   try {
+    // MetaMask requires requesting permission to connect users accounts
+    provider.send("eth_requestAccounts", []);
     await provider.ready;
   } catch (addError) {
     window.alert('Remove this app from your Connected Sites, via MetaMask desktop, and try again!');
